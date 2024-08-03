4b229c5a2335bff2ee0dc12b0c4a8718cc5dc069
Arthur Nunes <art.silvarn@gmail.com>
2024-08-02 22:39:05 -0300

    configuração do projeto spring

---
+++ src/test/java/com/ufla/sgrr/SgrrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufla.sgrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
