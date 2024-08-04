13ddead9ae8dc4ec301c91bc6166286c6d1c142e
Arthur Nunes <art.silvarn@gmail.com>
2024-08-04 16:51:51 -0300

    criando configuração de segurança de rotas

---
+++ src/main/java/com/ufla/sgrr/security/WebConfig.java
@@ -0,0 +1,29 @@
+package com.ufla.sgrr.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                ).csrf().disable();
+        return http.build();
+    }
+}
+
